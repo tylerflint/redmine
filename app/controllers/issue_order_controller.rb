fdda1001b7b7fea294ad6c222d795ce5dc2b3a36
Tyler Flint <tylerflint@Delorum10.local>
2010-05-13 09:59:46 -0600

    combining global list into project list; adding global sort interface functionality

--- app/controllers/issue_order_controller.rb
+++ app/controllers/issue_order_controller.rb
@@ -1,8 +1,6 @@
 class IssueOrderController < ApplicationController
   layout 'base'
   
-  before_filter :find_project, :only => :index
-  
   helper :journals
   helper :projects
   include ProjectsHelper   
@@ -21,8 +19,14 @@ class IssueOrderController < ApplicationController
   
   def index
     @projects = Project.visible.find(:all, :order => 'id, parent_id') 
+    project_id = (params[:issue] && params[:issue][:project_id]) || params[:project_id]
+    if project_id
+      @project = Project.find(project_id)
+      project_ids = get_project_ids.join ","
+    else
+      project_ids = get_member_project_ids.join ","
+    end
     @tree = create_project_tree
-    project_ids = get_project_ids.join ","
     @buckets = IssueBucket.all()
     @ordered_issues = Issue.find(:all, :conditions => "`order` > 0 AND `project_id` IN (#{project_ids}) AND status_id NOT IN (3,5,6)", :include => [:issue_bucket, :project], :order => "`order`")
     @unordered_issues = Issue.find(:all, :conditions => "(`order` = 0 OR `order` IS null) AND `project_id` IN (#{project_ids}) AND status_id NOT IN (3,5,6)", :include => [:issue_bucket, :project])
@@ -30,15 +34,6 @@ class IssueOrderController < ApplicationController
     respond_to do |format|
      format.html { render :template => 'issue_order/index.html.erb', :layout => !request.xhr? }
     end
-  end
-  
-  def global
-    @projects = Project.visible.find(:all, :order => 'id, parent_id') 
-    @tree = create_project_tree
-    project_ids = get_member_project_ids.join ","
-    @buckets = IssueBucket.all()
-    @ordered_issues = Issue.find(:all, :conditions => "`order` > 0 AND `project_id` IN (#{project_ids}) AND status_id NOT IN (3,5,6)", :include => [:issue_bucket, :project], :order => "`order`")
-    @unordered_issues = Issue.find(:all, :conditions => "(`order` = 0 OR `order` IS null) AND `project_id` IN (#{project_ids}) AND status_id NOT IN (3,5,6)", :include => [:issue_bucket, :project])
   end
   
   def add_bucket
@@ -78,17 +73,20 @@ class IssueOrderController < ApplicationController
   end
   
   def save_orders
-    params[:issues].split(",").each_with_index do |id, index|
-      Issue.update_all(['`order`=?', index+1], ['id=?', id])
-    end
-    if (params[:unordered_issues])
-      params[:unordered_issues].split(",").each do |id|
-        Issue.update_all(['`order`=?', 0], ['id=?', id])
-      end
-    end
-    render :update do |page|
-      page['success'].innerHTML = ""
-    end
+    params[:issue_id]
+    params[:previous_id]  # nil if issue_id is at the top of the list
+    params[:next_id]      # nil if issue_id is at the bottom of the list
+    # params[:issues].split(",").each_with_index do |id, index|
+    #   Issue.update_all(['`order`=?', index+1], ['id=?', id])
+    # end
+    # if (params[:unordered_issues])
+    #   params[:unordered_issues].split(",").each do |id|
+    #     Issue.update_all(['`order`=?', 0], ['id=?', id])
+    #   end
+    # end
+    # render :update do |page|
+    #   page['success'].innerHTML = ""
+    # end
   end
   
 private
